95f30bcb76216d537ddcd5bbce724c36930d620f
Tony <tonylyon0007@gmail.com>
2023-07-07 06:52:45 +0100

    hi

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_TEST, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
